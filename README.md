b5b059d9c5acac4b2117136721aa55c1a4db56ce
ponkotuy <web@ponkotuy.com>
2015-11-21 09:08:58 +0900

    Add host & protocol settings

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ sbt console
 get("myfleet.moe") // get simple
 get("google.com", "q" -> "myfleet") // with args
 post("hoge.com/session", "username" -> "ponkotuy", "password" -> "*****") // post form
-val session = post("hoge.com/session", ("username" -> "ponkotuy") ~ ("password" -> "*****") // post json from json4s
+val session = post("hoge.com/session", ("username" -> "ponkotuy") ~ ("password" -> "*****")) // post json from json4s
 session.get("hoge.com/image/1") // get with cookies
 get("myfleet.moe").res // get raw Response
+host = Host("ponkotuy.com")
+get("/index.html") // Use host settings
+protocol = Protocol("https")
+get("google.com", "q" -> "myfleet") // https protocol
 ```
 
 You can use head, put, delete options and trace.
--- src/main/scala/HttpHelper.scala
+++ src/main/scala/HttpHelper.scala
@@ -1,13 +1,11 @@
 
-import java.nio.charset.StandardCharsets
-
-import org.json4s._
-import org.json4s.native.JsonMethods._
 import skinny.http._
 import scala.collection.breakOut
 
 object HttpHelper extends HttpWrapper {
   override def cookie = Cookie.empty
+  implicit var host: Host = Host("localhost")
+  implicit var protocol: Protocol = Protocol("http")
 }
 
 case class Session(cookie: Cookie, res: Response, req: Request) extends HttpWrapper
@@ -32,91 +30,6 @@ object Cookie {
 
 case class HttpHelperException(mes: String) extends RuntimeException(mes)
 
-trait HttpWrapper {
-  import HttpWrapper._
+case class Host(name: String)
 
-  def cookie: Cookie
-
-  def get(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.GET, req)
-  }
-
-  def head(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.HEAD, req)
-  }
-
-  def post(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.POST, req)
-  }
-
-  def post(url: String, body: String): Session = {
-    val req = Request(fixURL(url)).body(body.getBytes(StandardCharsets.UTF_8))
-    request(Method.POST, req)
-  }
-
-  def post(url: String, json: JValue): Session = {
-    val req = Request(fixURL(url)).body(toBytes(json), "application/json; charset=utf-8")
-    request(Method.POST, req)
-  }
-
-  def put(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.PUT, req)
-  }
-
-  def put(url: String, body: String): Session = {
-    val req = Request(fixURL(url)).body(body.getBytes(StandardCharsets.UTF_8))
-    request(Method.PUT, req)
-  }
-
-  def put(url: String, json: JValue): Session = {
-    val req = Request(fixURL(url)).body(toBytes(json), "application/json; charset=utf-8")
-    request(Method.PUT, req)
-  }
-
-  def delete(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.DELETE, req)
-  }
-
-  def options(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.OPTIONS, req)
-  }
-
-  def trace(url: String, params: (String, String)*): Session = {
-    val req = Request(fixURL(url)).queryParams(params:_*).header("Cookie", cookie.toString)
-    request(Method.TRACE, req)
-  }
-
-  def request(method: Method, req: Request): Session = {
-    def f(req: Request): Response = {
-      val res = HTTP.request(method, req)
-      if(res.status / 100 == 3) {
-        res.header("Location").map { loc =>
-          f(req.copy(url = loc))
-        }.getOrElse(throw new HttpHelperException("Fail redirect"))
-      } else res
-    }
-    val res = f(req)
-    println(req.contentType)
-    printSummary(res)
-    val cookie = res.header("Set-Cookie").map(Cookie.fromStr).getOrElse(Cookie.empty)
-    Session(cookie, res, req)
-  }
-}
-
-object HttpWrapper {
-  private def fixURL(url: String) = if(url.contains("//")) url else "http://" + url
-
-  private def printSummary(res: Response): Unit = {
-    println(res.header(null).getOrElse(""))
-    if(res.asString.length > 100) println(res.asString.take(100) + "...") else println(res.asString)
-    println()
-  }
-
-  private def toBytes(json: JValue): Array[Byte] = pretty(render(json)).getBytes(StandardCharsets.UTF_8)
-}
+case class Protocol(name: String)
